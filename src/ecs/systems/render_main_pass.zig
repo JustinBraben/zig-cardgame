ad23aa51e38ea1db1a0dfbbe7cdb3c7b6f95f9e4
Justin Braben <jbraben@dmsiworks.com>
2024-04-03 12:56:17 -0600

    update

--- src/ecs/systems/render_main_pass.zig
+++ src/ecs/systems/render_main_pass.zig
@@ -9,72 +9,67 @@ const Components = @import("../components/components.zig");
 const utils = @import("../../utils.zig");
 
 pub fn run(gamestate: *GameState) !void {
-    const uniforms = gfx.UniformBufferObject{
-        .mvp = zmath.transpose(gamestate.camera.frameBufferMatrix()),
+    var uniforms = gfx.UniformBufferObject{
+        .mvp = zmath.transpose(gamestate.camera.renderTextureMatrix()),
     };
 
-    // Render the playing table
-    // try gamestate.batcher.begin(.{
-    //     .pipeline_handle = gamestate.pipeline_game_window,
-    //     .bind_group_handle = gamestate.bind_group_game_window,
-    // });
-    // // Draw the playing table texture
-    // // const pos = zmath.f32x4(-640, -320, 100, 0);
-    // const pos = gamestate.camera.worldToScreen(
-    //     zmath.f32x4(
-    //         -1.0 * (@as(f32, @floatFromInt(game.settings.design_width)) / 2.0),
-    //         (@as(f32, @floatFromInt(game.settings.design_height)) / 2.0),
-    //         0,
-    //         0
-    //         )
-    //     );
-    // try gamestate.batcher.texture(pos, &gamestate.game_window_texture, .{});
-    // try gamestate.batcher.end(uniforms, gamestate.uniform_buffer_default);
-
-    // Render the sprites
-    try gamestate.batcher.begin(.{
-        .pipeline_handle = gamestate.pipeline_default,
-        .bind_group_handle = gamestate.bind_group_default,
-        // .output_handle = gamestate.default_output.view_handle,
-    });
-    var view = gamestate.world.view(.{ Components.Tile, Components.SpriteRenderer }, .{});
-    var iter = view.entityIterator();
-    while (iter.next()) |entity| {
-        const tile = view.getConst(Components.Tile, entity);
-        // const tile_pos = utils.tileToPixelCoords(tile);
-        // const position = utils.toF32x4(tile_pos);
-        const position = zmath.f32x4(
-            @as(f32, @floatFromInt(tile.x)) * game.settings.pixels_per_unit_x,
-            @as(f32, @floatFromInt(tile.y)) * game.settings.pixels_per_unit_y,
-            @as(f32, @floatFromInt(tile.z)) * 32, 
-            0
-        );
-        
-        const renderer = view.getConst(Components.SpriteRenderer, entity);
-        gamestate.batcher.sprite(
-            position, 
-            &gamestate.default_texture,
-            gamestate.atlas.sprites[renderer.index],
-            .{
-                .time = gamestate.game_time + @as(f32, @floatFromInt(renderer.order)),
-                .rotation = 0.0,
-                .flip_x = false,
-                .flip_y = false,
-            },
-        ) catch unreachable;
+    {
+        // Render the playing table to default output texture
+        try gamestate.batcher.begin(.{
+            .pipeline_handle = gamestate.pipeline_game_window,
+            .bind_group_handle = gamestate.bind_group_game_window,
+            .output_handle = gamestate.default_output.view_handle,
+        });
+        const position = zmath.f32x4s(0);
+        gamestate.batcher.texture(position, &gamestate.game_window_texture, .{}) catch unreachable;
+        try gamestate.batcher.end(uniforms, gamestate.uniform_buffer_default);
     }
 
+
+    {
+        // Render the sprites to the default output texture
+        try gamestate.batcher.begin(.{
+            .pipeline_handle = gamestate.pipeline_default,
+            .bind_group_handle = gamestate.bind_group_default,
+            .output_handle = gamestate.default_output.view_handle,
+        });
+        var view = gamestate.world.view(.{ Components.Tile, Components.SpriteRenderer }, .{});
+        var iter = view.entityIterator();
+        while (iter.next()) |entity| {
+            const tile = view.getConst(Components.Tile, entity);
+            // const tile_pos = utils.tileToPixelCoords(tile);
+            // const position = utils.toF32x4(tile_pos);
+            const position = zmath.f32x4(
+                (@as(f32, @floatFromInt(tile.x)) * game.settings.pixels_per_unit_x),
+                (@as(f32, @floatFromInt(tile.y)) * game.settings.pixels_per_unit_y),
+                @as(f32, @floatFromInt(tile.z)) * 32, 
+                0
+            );
+            const renderer = view.getConst(Components.SpriteRenderer, entity);
+            gamestate.batcher.sprite(
+                position, 
+                &gamestate.default_texture,
+                gamestate.atlas.sprites[renderer.index],
+                .{
+                    .time = gamestate.game_time + @as(f32, @floatFromInt(renderer.order)),
+                    .rotation = 0.0,
+                    .flip_x = false,
+                    .flip_y = false,
+                },
+            ) catch unreachable;
+        }
+        try gamestate.batcher.end(uniforms, gamestate.uniform_buffer_default);
+    }
+
+    uniforms = gfx.UniformBufferObject{ 
+        .mvp = zmath.transpose(gamestate.camera.frameBufferMatrix()),
+    };
+    try gamestate.batcher.begin(.{
+        .pipeline_handle = gamestate.pipeline_default_output,
+        .bind_group_handle = gamestate.bind_group_default_output,
+    });
+    try gamestate.batcher.texture(zmath.f32x4s(0), &gamestate.default_output, .{});
     try gamestate.batcher.end(uniforms, gamestate.uniform_buffer_default);
-
-    // try gamestate.batcher.begin(.{
-    //     .pipeline_handle = gamestate.pipeline_default,
-    //     .bind_group_handle = gamestate.bind_group_default,
-    //     .output_handle = gamestate.default_output.view_handle,
-    // });
-
-    // try gamestate.batcher.texture(zmath.f32x4s(0), &gamestate.default_output, .{});
-
-    // try gamestate.batcher.end(uniforms, gamestate.uniform_buffer_default);
 }
 
 pub fn renderSprites(gamestate: *GameState) !void {
